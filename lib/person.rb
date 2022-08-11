00ada1f427f0a32b283f412c51faa93f29843cf0
Nick Warren <nickwarren47@gmail.com>
2022-08-10 19:58:31 -0600

    commit tree 48c06dd783e5ed71f9fe5f4a3b93d8720f835c78

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+    # setter method
+    def name=(name)
+        @name=name
+    end
+    # getter method
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job=job
+    end
+
+    def job
+        @job
+    end
 end
+
+kanye = Person.new
+kanye.name = "Kanye"
+kanye.name
